1179c02a5ec3521bf53e04e69bc0600e7e706a62
Luis Antonio Canettoli <hello@luisanton.io>
2020-12-04 14:52:35 +0100

    First Commit

---
+++ src/RecoilNexus.tsx
@@ -0,0 +1,71 @@
+import {
+    RecoilState,
+    useRecoilCallback
+} from 'recoil';
+import { Subject } from 'rxjs';
+
+type RecoilInjection<T> = {
+    atom: RecoilState<T>,
+    value: T
+}
+
+type RecoilRetrieval<T> = {
+    atom: RecoilState<T>,
+    subject: Subject<T>
+}
+
+const nexus = {
+    get: new Subject<RecoilRetrieval<any>>(),
+    set: new Subject<RecoilInjection<any>>()
+}
+
+export default function RecoilNexus() {
+
+    const getAtom = useRecoilCallback(({ snapshot }) =>
+        async function <T>(atom: RecoilState<T>, subject: Subject<T>) {
+            const value = await snapshot.getPromise(atom);
+            subject.next(value)
+        }, [])
+
+    nexus.get.subscribe({ next: ({atom, subject}) => getAtom(atom, subject) })
+
+    const setAtom = useRecoilCallback(({ set }) =>
+        function <T>(atom: RecoilState<T>, value: T) {
+            set(atom, value)
+        }, [])
+
+    nexus.set.subscribe({ next: ({ atom, value }) => setAtom(atom, value) });
+
+    return null
+}
+
+
+export function useRecoilNexusValue<T>(atom: RecoilState<T>): () => Promise<T> {
+    
+    return () => new Promise<T>(
+        async function (resolve) {
+            const temporary = new Subject<T>()
+            temporary.subscribe({
+                next: (value) => resolve (value)
+            });
+            nexus.get.next({atom, subject: temporary});
+        }
+    )
+}
+
+export function useRecoilNexusSetValue<T>(atom: RecoilState<T>): (v: T) => void {
+
+    return (value: T) => nexus.set.next(
+        { atom, value } as RecoilInjection<T>
+    )
+
+}
+
+export function useRecoilNexus<T>(atom: RecoilState<T>): [() => Promise<T>, (v: T) => void] {
+
+    return [
+        useRecoilNexusValue(atom),
+        useRecoilNexusSetValue(atom)
+    ]
+
+}
